75229362d416490ea6debca352354ccdd9b249b4
samsax53 <86681423+samsax53@users.noreply.github.com>
2021-07-09 17:33:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+menu_list_array = ["Veg Margherita Pizza", "Golden Corn Pizza", "Veg Dexule Pizza", "Mexican Pizza", "Pepperoni Pizza"];
+
+function getmenu(){
+    var htmldata;
+htmldata="<ol class='menulist'>";
+menu_list_array.sort();
+for(i=0; i < menu_list_array.length; i++) {
+    htmldata=htmldata+'<li>'+ menu_list_array[i] + '</li>'
+}
+htmldata= htmldata + "</ol>";
+document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+htmldata="<class='cards'>"
+
+for(i=0; i < menu_list_array.length; i++) {
+    htmldata = htmldata + '<div class="card">' + '<img src="pizzaImg.png"/>' + menu_list_array[i] + '</div>'
+}
+htmldata = htmldata + "</section>"
+document.getElementById("display_addedmenu").innerHTML = htmldata;
+
+}
+
+function add_top(){
+var item = document.getElementById("add_item").value;
+menu_list_array.push(item);
+add_item();
+}
